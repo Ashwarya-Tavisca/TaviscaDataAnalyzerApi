f7e9637e99653633fea031a7456eecab0ed38ea7
Bhargavi Deshpande <bdeshpande@tavisca.com>
2018-10-26 13:28:20 +0530

    Implement send PDF attached email

--- DataAnalyzerToolApi/TaviscaDataAnalyzerServiceProvider/EmailService.cs
+++ DataAnalyzerToolApi/TaviscaDataAnalyzerServiceProvider/EmailService.cs
@@ -4,6 +4,10 @@ using System.Net;
 using System.Net.Mail;
 using System.Text;
 using CoreContracts.Models;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using iTextSharp.text.html.simpleparser;
+using System.IO;
 
 namespace TaviscaDataAnalyzerServiceProvider
 {
@@ -11,49 +15,64 @@ namespace TaviscaDataAnalyzerServiceProvider
     {
         public void generatingMail(RecipientDetails details)
         {
-            string textBody = "";
+            string reportBody = "";
             for (int tableCount = 0; tableCount < details.Labels.Length; tableCount++)
             {
-                textBody = textBody + "<h2> Analysis Report of " + details.FilterName[tableCount] + "</h2>" +
+                reportBody += "<h2> Analysis Report of " + details.FilterName[tableCount] + "</h2>" +
                                "<h3> from " + details.StartDate[tableCount] + " upto " + details.EndDate[tableCount] + " at " + details.Location[tableCount] + "</h3>" + "<br>" +
                                " <table border=" + 1 + " cellpadding=" + 10 + " cellspacing=" + 0 + " width = " + 500 + ">" +
                                "<tr bgcolor='#D3D3D3'>" +
                                "<td><b>Lables</b></td> " +
                                "<td><b>Statistics</b></td>" +
                                "</tr>";
-            for (int loopCount = 0; loopCount < details.Labels[tableCount].Length; loopCount++)
-            {
-                textBody += "<tr>" +
-                            "<td>" + details.Labels[tableCount][loopCount] + "</td>" +
-                            "<td> " + details.Statistics[tableCount][loopCount] + "</td> " +
-                            "</tr>";
+                for (int RowCount = 0; RowCount < details.Labels[tableCount].Length; RowCount++)
+                {
+                    reportBody += "<tr>" +
+                    "<td>" + details.Labels[tableCount][RowCount] + "</td>" +
+                    "<td> " + details.Statistics[tableCount][RowCount] + "</td> " +
+                    "</tr>";
+                }
+                reportBody += "</table>" + "<br>";
             }
-            textBody += "</table>" + "<br>";
-        }
 
 
-        MailMessage mailMessage = new MailMessage("purab2018@gmail.com", details.RecipientEmialId);
-            mailMessage.Subject = "Data Analysis Report";
-            mailMessage.Body = textBody;
-            mailMessage.IsBodyHtml = true;
+            StringReader sr = new StringReader(reportBody);
+            Document pdfDoc = new Document(PageSize.A4, 50f, 10f, 20f, 0f);
+            HTMLWorker htmlparser = new HTMLWorker(pdfDoc);
 
-            NetworkCredential credentials = new NetworkCredential();
-            credentials.UserName = "purab2018@gmail.com";
-            credentials.Password = "Tavisca@123";
-
-            SmtpClient client = new SmtpClient();
-            client.Host = "smtp.gmail.com";
-            client.Credentials = credentials;
-            client.Port = 587;
-            client.EnableSsl = true;
-
-            try
+            using (MemoryStream memoryStream = new MemoryStream())
             {
-                client.Send(mailMessage);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.ToString());
+                PdfWriter writer = PdfWriter.GetInstance(pdfDoc, memoryStream);
+                pdfDoc.Open();
+                htmlparser.Parse(sr);
+                pdfDoc.Close();
+                byte[] bytes = memoryStream.ToArray();
+                memoryStream.Close();
+
+                MailMessage mailMessage = new MailMessage("purab2018@gmail.com", details.RecipientEmialId);
+                mailMessage.Subject = "Data Analysis Report";
+                mailMessage.Body = "Your analysis report is attached with this Email !";
+                mailMessage.IsBodyHtml = true;
+                mailMessage.Attachments.Add(new Attachment(new MemoryStream(bytes), "DataAnalysisReport.pdf"));
+
+                NetworkCredential credentials = new NetworkCredential();
+                credentials.UserName = "purab2018@gmail.com";
+                credentials.Password = "Tavisca@123";
+
+                SmtpClient client = new SmtpClient();
+                client.Host = "smtp.gmail.com";
+                client.Credentials = credentials;
+                client.Port = 587;
+                client.EnableSsl = true;
+
+                try
+                {
+                    client.Send(mailMessage);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
             }
         }
     }
